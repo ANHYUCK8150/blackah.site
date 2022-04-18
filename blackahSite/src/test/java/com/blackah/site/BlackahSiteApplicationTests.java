3cbb6340850fc36754e51da2dbea96986ee60c2b
opilk <opilk@DESKTOP-OU5TTRR>
2022-04-18 16:22:28 +0900

    22.04.18 test 코드 제거

--- blackahSite/src/test/java/com/blackah/site/BlackahSiteApplicationTests.java
+++ blackahSite/src/test/java/com/blackah/site/BlackahSiteApplicationTests.java
@@ -1,7 +1,5 @@
 package com.blackah.site;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -13,12 +11,6 @@ class BlackahSiteApplicationTests {
 	void contextLoads() {
 	}
 	
-	@Test
-	public void bookList(HttpServletRequest request) {
-		//System.out.println(bookService.searchBook("java",10,1));
-		String root_path = request.getSession().getServletContext().getRealPath("/"); 
-		System.out.println(root_path);
-	}
 	
 
 }
